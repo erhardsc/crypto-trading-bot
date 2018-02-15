b5f5338022b93cbf7e44288cce368128edf23b95
Sumner <serhard2556@gmail.com>
2018-02-15 01:54:25 -0700

    Basic plotting still doesn't work. Having trouble getting matplotlib to read the x(dates) and y(prices) axis

--- backtest.py
+++ backtest.py
@@ -1,23 +1,37 @@
 import sys
 import getopt
-
+import datetime as dt
 from botchart import BotChart
 from botstrategy import BotStrategy
-import botlog as output
 import pandas as pd
 import config
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import numpy as np
 
 
 # Testing historical price data.
 def main(argv):
-    startTime = 1504224000
+    startTime = 1502224000
     endTime = 1506815999
-    chart = BotChart(startTime, endTime, "poloniex", "BTS/BTC", "1d")
+    pair = "ETH/BTC"
+    chart = BotChart(startTime, endTime, "poloniex", pair, "1d")
     strategy = BotStrategy()
 
     for candlestick in chart.getPoints():
         strategy.tick(candlestick)
 
+    graph(pair)
+
+
+def graph(pair):
+    path = config.CONSTS['PATH']
+    plt.title(pair)
+    df = pd.read_csv(path + "/data/indicators.csv", parse_dates=True, infer_datetime_format=True, index_col=0)
+    date = df["Date"]
+    dateconv = np.vectorize(dt.datetime.fromtimestamp)
+    date = dateconv(date)
+    plt.show()
 
 if __name__ == "__main__":
     main(sys.argv[1:])
--- botlog.py
+++ botlog.py
@@ -1,13 +1,6 @@
-import matplotlib.pyplot as plt
-
-
 class BotLog(object):
     def __init__(self):
         pass
 
     def log(self, message):
         print(message)
-
-    def graph(self, df):
-        df.plot()
-        plt.show()
